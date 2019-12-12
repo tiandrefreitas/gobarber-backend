d7c5049802946b119ec25e519574ad44a52db246
Andre Freitas <ti.andrefreitas@gmail.com>
2019-12-12 20:07:00 -0300

    alter env

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
-  host: '127.0.0.1',
-  username: 'postgres',
-  password: 'alidila9',
-  database: 'gobarber',
+  host: process.env.DATABASE_HOST,
+  username: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE_NAME,
   define: {
     timestamps: true,
     underscored: true,
