25932f67fb546fa23b9658c2878ab1025ed71cf5
Luis Freitas Da Silva <aluisf@indracompany.com>
2019-12-13 09:54:59 -0300

    update

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,11 @@
-	require('dotenv/config');
+require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
-<<<<<<< HEAD
-  host: process.env.DATABASE_URL,
-  username: process.env.USER_NAME,
-  password: process.env.PASSWORD,
-  database: 'gobarber',
-=======
   host: process.env.DATABASE_HOST,
   username: process.env.USER_NAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE_NAME,
->>>>>>> f50c2e595ae5a12cf264d526fa7fcfe9c4e3c897
   define: {
     timestamps: true,
     underscored: true,
