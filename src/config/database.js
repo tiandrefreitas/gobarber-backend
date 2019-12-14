10780fdb416084b4eb309c06ffd5fc46e24046b3
Andre Freitas <ti.andrefreitas@gmail.com>
2019-12-14 14:44:09 -0300

    update

--- src/config/database.js
+++ src/config/database.js
@@ -2,16 +2,10 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
-<<<<<<< HEAD
-  host: process.env.DATABASE_HOST,
-  username: process.env.USER_NAME,
-  password: process.env.PASSWORD,
-=======
   host: process.env.DATABASE_URL,
   username: process.env.USER_NAME,
   password: process.env.PASSWORD,
   database: 'gobarber',
->>>>>>> 275ba4d90963b29341e261e42bc15b604ea37247
   define: {
     timestamps: true,
     underscored: true,
