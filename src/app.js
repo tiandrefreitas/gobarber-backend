134c9b16351b95362ebdc9efdd730daf498b3c95
Luis Freitas Da Silva <aluisf@indracompany.com>
2019-12-19 09:08:03 -0300

    update

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ class App {
         ],
       })
     );
-    this.server.use.options('*', cors());
+    this.server.options('*', cors());
     this.server.use.all('', function(req, res, next) {
       res.header(
         'Access-Control-Allow-Origin',
