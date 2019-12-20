36df062114fc67072d31c88fb97cfc05d2ed8f3e
Luis Freitas Da Silva <aluisf@indracompany.com>
2019-12-20 11:58:01 -0300

    update

--- src/database/migrations/20190625182637-add-avatar-field-to-users.js
+++ src/database/migrations/20190625182637-add-avatar-field-to-users.js
@@ -2,6 +2,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('users', 'avatar_id', {
       type: Sequelize.INTEGER,
+      defaultValue: 1,
       references: { model: 'files', key: 'id' },
       onUpdate: 'CASCADE',
       onDelete: 'SET NULL',
